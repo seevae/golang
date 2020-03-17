9977a77fca43fe21864ceaf6f3f3b3f37768e561
seevae <906435177@qq.com>
2020-03-17 18:45:24 +0800

    for练习题

--- hello_golang/src/go_code_02/hello_crc.go
+++ hello_golang/src/go_code_02/hello_crc.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	/**
@@ -13,7 +15,7 @@ func main() {
 	//for循环代替while循环的写法--》省略1,3表达式，只写判断条件
 	num := 1
 	for num >= 1 { //如果fo后面什么条件也不加那么默认为true 死循环
-		println(num)
+		fmt.Println(num)
 		num++
 		if num == 5 {
 			break
@@ -45,7 +47,67 @@ func main() {
 	fmt.Print("\n")
 	fmt.Printf("总共有%d个符合要求的数字\n", count)
 
-	printPattern()
+	printPattern() //打印菱形
+	function1()    //打印乘法口诀表
+	function2()    //水仙花数
+	function3()    //打印素数
+	function4()  //goto语句的练习-->一般不使用
+}
+
+//goto
+func function4(){
+	for i:=0;i<5;i++{
+		for j:=0;j<6;j++{
+			if j == 2 {
+				goto breakHere
+			}
+		}
+	}
+breakHere:
+	fmt.Println("done")
+}
+
+//打印素数
+func function3() {
+	for i := 2; i <= 100; i++ {
+		flag := true
+		for j := 2; j < i; j++ {
+			if i%j == 0 {
+				flag = false
+				break
+			}
+		}
+		if flag {
+			fmt.Print(i, " ")
+		}
+	}
+	fmt.Println()
+}
+
+//水仙花数
+func function2() {
+	for i := 100; i <= 999; i++ {
+		x := i % 10       //个位
+		y := i % 100 / 10 //十位
+		z := i / 100      //百位
+		if x*x*x+y*y*y+z*z*z == i {
+			fmt.Print(i, "\n")
+		}
+		//函数替换上述立法计算
+		//if math.Pow(float64(x),3)+math.Pow(float64(y),3)+math.Pow(float64(z),3) ==float64(i){
+		//	fmt.Print(i,"\n")
+		//}
+	}
+}
+
+//乘法口诀表
+func function1() {
+	for i := 1; i <= 9; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%d*%d=%d ", j, i, j*i)
+		}
+		fmt.Printf("\n")
+	}
 }
 
 //打印菱形
