5fc7d410b55e8ffbdd8da9a852f387d7f99c60a1
seevae <906435177@qq.com>
2020-03-26 10:40:05 +0800

    map and slice

--- hello_golang/src/go_code_05/mapTest.go
+++ hello_golang/src/go_code_05/mapTest.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	/*
@@ -49,28 +51,65 @@ func main() {
 	/**如何区分返回的值是默认的值还是map中已经存放的值, 例如map1中放入一个空字符串,此时取map1[40]还是返回一个空字符串
 		那究竟是哪个字符串呢,此时就要用到两个返回值来做判断
 	**/
-	v1,ok := map1[40]
-	if ok == true{
-		fmt.Println("key存在,value=",v1)
-	}else {
-		fmt.Println("key不存在,返回的是默认值:",v1)
+	v1, ok := map1[40]
+	if ok == true {
+		fmt.Println("key存在,value=", v1)
+	} else {
+		fmt.Println("key不存在,返回的是默认值:", v1)
 	}
 	//map的长度计算  len(map) --> 求出来的就是map的长度及对数
 	len1 := len(map1)
 	fmt.Println(len1)
 	//删除数据
-	delete(map1,4) //key值存在直接删除.key值不存在删除失败
+	delete(map1, 4) //key值存在直接删除.key值不存在删除失败
 	fmt.Println(map1)
 
 	//map的遍历
 	//for..range遍历: 数组中获取的是下标和对应的值,map中获取的是key和对应的value
-	for key,value:=range map1{
-		fmt.Println(key,value)  //打印结果是无序的.
+	for key, value := range map1 {
+		fmt.Println(key, value) //打印结果是无序的.
 	}
 	//有序的遍历map,for循环
-	for i:=1;i<= len(map1);i++{
-		fmt.Println(i,map1[i])
+	for i := 1; i <= len(map1); i++ {
+		fmt.Println(i, map1[i])
 	}
+
+	/**
+	1.创建map用于存储人的信息
+		name,age,sex,address
+	2.每个map存储一个人的信息
+	3.将这些map放入一个slice中
+	4.打印遍历输出
+	*/
+	peopleSlice := make([]map[string]string, 0, 0)
+	map11 := make(map[string]string)
+	map11["name"] = "张益达"
+	map11["age"] = "12"
+	map11["sex"] = "男"
+	map11["address"] = "北京"
+
+	map12 := make(map[string]string)
+	map12["name"] = "李菲菲"
+	map12["age"] = "12"
+	map12["sex"] = "男"
+	map12["address"] = "北京"
+
+	map13 := make(map[string]string)
+	map13["name"] = "赵小雨"
+	map13["age"] = "13"
+	map13["sex"] = "女"
+	map13["address"] = "北京"
+
+	peopleSlice = append(peopleSlice, map11,map12,map13)
+	for i,value:=range peopleSlice{
+		fmt.Printf("第 %d 个人的信息是:\n",i+1)
+		fmt.Printf("\t姓名:%s\n",value["name"])
+		fmt.Printf("\t年龄:%s\n",value["age"])
+		fmt.Printf("\t性别:%s\n",value["sex"])
+		fmt.Printf("\t地址:%s\n",value["address"])
+	}
+
+	//map也是引用类型
 
 
 }
